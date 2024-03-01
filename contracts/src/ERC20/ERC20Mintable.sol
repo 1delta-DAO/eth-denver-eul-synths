774cf95ea120d8d4417ccaf869779953873ae611
caglavol <18139560+caglavol@users.noreply.github.com>
2024-03-01 09:23:40 -0700

    make token contract burnable and add metadata

--- contracts/src/ERC20/ERC20Mintable.sol
+++ contracts/src/ERC20/ERC20Mintable.sol
@@ -10,11 +10,16 @@ import "openzeppelin/access/Ownable.sol";
 /// @title ERC20Collateral
 /// @notice It extends the ERC20 token standard to add the EVC authentication and account status checks so that the
 /// token contract can be used as collateral in the EVC ecosystem.
-contract ERC20Mintable is ERC20, ERC20Burnable, ReentrancyGuard, Ownable {
+contract ERC20Mintable is ERC20, Ownable {
+    uint8 private decimal;
+
     constructor(
         string memory _name_,
-        string memory _symbol_
-    ) ERC20(_name_, _symbol_) ERC20Burnable() Ownable(msg.sender) {}
+        string memory _symbol_,
+        uint8 _decimal
+    ) ERC20(_name_, _symbol_) Ownable(msg.sender) {
+        decimal = _decimal;
+    }
 
     /**
      * @dev Extension of {ERC20} that adds a set of accounts with the {OwnerRole},
@@ -26,5 +31,18 @@ contract ERC20Mintable is ERC20, ERC20Burnable, ReentrancyGuard, Ownable {
     ) public onlyOwner returns (bool) {
         _mint(account, amount);
         return true;
+    }
+
+    function burn(address from, uint256 value) public onlyOwner {
+        _burn(from, value);
+    }
+
+    function burnFrom(address account, uint256 value) public onlyOwner {
+        _spendAllowance(account, _msgSender(), value);
+        _burn(account, value);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return decimal;
     }
 }
