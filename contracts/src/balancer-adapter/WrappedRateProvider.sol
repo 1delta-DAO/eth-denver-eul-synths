09fae3184706c572c28f628c72f65536e9e4b74e
Achthar <69119535+Achthar@users.noreply.github.com>
2024-03-01 13:27:03 -0700

    complete balancer adapter test

--- contracts/src/balancer-adapter/WrappedRateProvider.sol
+++ contracts/src/balancer-adapter/WrappedRateProvider.sol
@@ -7,18 +7,22 @@ import {AggregatorV3Interface} from "./interfaces/IAggregator.sol";
 contract WrappedRateProvider is IRateProvider {
     address private immutable CHAIN_LINK_AGGREGATOR;
 
+    // multiplying a ChainLink pice with this makes it 18 decimals
+    uint256 PRICE_SCALE = 10 ** (18 - 8);
+
     constructor(address chainLinkAggregator) {
         CHAIN_LINK_AGGREGATOR = chainLinkAggregator;
     }
 
     function getRate() external view override returns (uint256 rate) {
         (
-         ,   // uint80 roundId,
-            int256 answer,
-         ,   // uint256 startedAt,
-         ,   // uint256 updatedAt,
-             // uint80 answeredInRound
+            ,
+            // uint80 roundId,
+            int256 answer, // uint256 startedAt, // uint256 updatedAt, // uint80 answeredInRound
+            ,
+            ,
+
         ) = AggregatorV3Interface(CHAIN_LINK_AGGREGATOR).latestRoundData();
-        return uint256(answer);
+        return uint256(answer) * PRICE_SCALE;
     }
 }
--- contracts/test/balancer-adapter/BalancerAdapter.t.sol
+++ contracts/test/balancer-adapter/BalancerAdapter.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.19;
 
-import "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "evc/EthereumVaultConnector.sol";
 import "../../src/vaults/solmate/VaultSimple.sol";
@@ -34,6 +34,10 @@ contract BalancerAdapterTest is
     IBalancerVaultGeneral balancerVault;
 
     uint256 constant MAX_VAL = type(uint).max;
+
+    // we use this to track all deposits for stables (with 18 decimals)
+    // it provides a benchmark for USD deposit value
+    uint256 APPROX_PRICE_TRACKER;
 
     function setUp() public {
         vm.createSelectFork({
@@ -76,30 +80,86 @@ contract BalancerAdapterTest is
     function test_adapter_join_cs_pool() public {
         create();
         init();
+        address pool = balancerAdapter.pool();
+        uint balance = IERC20(pool).balanceOf(address(this));
+        // deposit balances to pool
+        joinPool();
+
+        balance = IERC20(pool).balanceOf(address(this)) - balance;
+        console.log("balance received", balance);
+        // we assert that enough BPTs were minted
+        assert(balance >= 3119.0e18);
+    }
+
+    function test_adapter_pricing() public {
+        create();
+        init();
+        address pool = balancerAdapter.pool();
+        // deposit balances to pool
+        joinPool();
+
+        // this is the total supply
+        uint balance = IERC20(pool).balanceOf(address(this));
+        uint256 price = balancerAdapter.getPrice();
+
+        console.log("price in USD", price);
+
+        address quoteAsset = address(USDC);
+        uint256 quoteAll = balancerAdapter.getQuote(
+            balance,
+            address(0),
+            quoteAsset
+        );
+
+        assertApproxEqAbs(
+            quoteAll * 1e12,
+            APPROX_PRICE_TRACKER,
+            (APPROX_PRICE_TRACKER * 1) / 100 // allow 5% deviation
+        );
+        uint256 halfBalance = balance / 2;
+        uint256 quote = balancerAdapter.getQuote(
+            halfBalance,
+            address(0),
+            quoteAsset
+        );
+        console.log("quote in USDC", quote);
+
+        quoteAsset = address(DAI);
+        quoteAll = balancerAdapter.getQuote(balance, address(0), quoteAsset);
+
+        assertApproxEqAbs(
+            quoteAll,
+            APPROX_PRICE_TRACKER,
+            (APPROX_PRICE_TRACKER * 1) / 100 // allow 5% deviation
+        );
+
+        console.log("quoteAll in DAI", quoteAll);
+        halfBalance = balance / 2;
+        quote = balancerAdapter.getQuote(halfBalance, address(0), quoteAsset);
+        console.log("quote in DAI", quote);
+    }
+
+    function joinPool() internal {
         uint[] memory amounts = new uint256[](3);
         address adapter = address(balancerAdapter);
 
         uint eusdAmount = 1_200.0e18;
+        APPROX_PRICE_TRACKER += eusdAmount;
         amounts[0] = eusdAmount;
         eUSD.transfer(adapter, eusdAmount);
 
         uint usdcAmount = 1_020.0e6;
+        APPROX_PRICE_TRACKER += usdcAmount * 1e12;
         amounts[1] = usdcAmount;
         USDC.transfer(adapter, usdcAmount);
 
         uint daiAmount = 900.0e18;
+        APPROX_PRICE_TRACKER += daiAmount;
         amounts[2] = daiAmount;
         DAI.transfer(adapter, daiAmount);
         console.log("join");
-        address pool = balancerAdapter.pool();
-        uint balance = IERC20(pool).balanceOf(address(this));
         // deposit balances to pool
         balancerAdapter.depositTo(amounts, address(this));
-
-        balance = IERC20(pool).balanceOf(address(this)) - balance;
-        console.log(balance);
-        // we assert that enough BPTs were minted
-        assert(balance >= 215105030921280412);
     }
 
     function init() private {
@@ -108,14 +168,17 @@ contract BalancerAdapterTest is
         address adapter = address(balancerAdapter);
 
         uint eusdAmount = 10.0e18;
+        APPROX_PRICE_TRACKER += eusdAmount;
         amounts[1] = eusdAmount;
         eUSD.transfer(adapter, eusdAmount);
 
         uint usdcAmount = 10.0e6;
+        APPROX_PRICE_TRACKER += usdcAmount * 1e12;
         amounts[2] = usdcAmount;
         USDC.transfer(adapter, usdcAmount);
 
         uint daiAmount = 10.0e18;
+        APPROX_PRICE_TRACKER += daiAmount;
         amounts[3] = daiAmount;
         DAI.transfer(adapter, daiAmount);
 
