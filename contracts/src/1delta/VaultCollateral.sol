fa10281f0930a06c6a635a49120366ab64b0416f
caglavol <18139560+caglavol@users.noreply.github.com>
2024-03-01 18:07:53 -0700

    create wrapper VaultCollateral for VaultSimple

---
+++ contracts/src/1delta/VaultCollateral.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.19;
+
+import {VaultSimple, ERC20, IEVC} from "../vaults/open-zeppelin/VaultSimple.sol";
+
+contract VaultCollateral is VaultSimple {
+    constructor(
+        IEVC _evc,
+        address _asset,
+        string memory _name,
+        string memory _symbol
+    ) VaultSimple(_evc, ERC20(_asset), _name, _symbol) {}
+}
